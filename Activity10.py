556a3e2e0478382a7bc0e1e11ece764496c6d9d3
Aayansh889 <aayansh.saxena@gmail.com>
2025-12-27 21:46:10 +0530

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,8 @@
+wear=input("Enter what kind you want to wear(light or dark):")
+
+if wear=="light":
+    print("This should be worn in summers")
+elif wear=="dark":
+    print("This should be worn in winters")
+else:
+    print("Not from given choices")
